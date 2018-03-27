61ec660fd33c90074a82c90312acaab8305230d1
Qin TIAN <Qin TIAN>
2018-03-27 09:27:48 -0700

    nothing added

---
+++ source/softmax.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+""" Short description of this Python module.
+Longer description of this module.
+This program is for: .
+"""
+
+__authors__ = ["Qin Tian<qtian.whu@gmail.com>", "Ming Jin<jinming@berkeley.edu>", "Wei Feng<weifeng@lbl.gov>"]
+__contact__ = "Wei Feng"
+__copyright__ = "Copyright 2018, CHINA ENERGY GROUP@LBNL"
+__date__ = "18-1-22"
+__deprecated__ = False
+__email__ = "weifeng@lbl.gov"
+__license__ = "GPLv3"
+__maintainer__ = "Qin Tian, Ming Jing"
+__status__ = "Production"
+__version__ = "2.0.0"
